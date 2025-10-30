6bc8dbc9450b7082bd49bdfe5548a86de14aa27e
itsmeenattt <nadiansthva31@gmail.com>
2025-10-30 16:26:48 +0700

    feat: implemented functional login button with alert

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,11 @@
+// src/components/AppHeader.jsx
 import './AppHeader.css';
+
+// Fungsi Handler untuk Login/Menu
+const handleNavClick = (featureName) => {
+    alert(`Fitur ${featureName} belum diimplementasikan untuk UTS ini.`);
+    console.log(`Navigasi diklik: ${featureName}`);
+};
 
 function AppHeader({ onToggleFilter, isFilterActive }) { 
     const heroImageUrl = 'https://images.unsplash.com/photo-1536440136659-142a78dc1241?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
@@ -6,16 +13,16 @@ function AppHeader({ onToggleFilter, isFilterActive }) {
     return (
         <header className="hero-section" style={{ backgroundImage: `url(${heroImageUrl})` }}>
             
-            {}
+            {/* Navbar Utama */}
             <div className="navbar">
                 <div className="container navbar-content">
                     <div className="navbar-left">
-                        {}
                         <div className="logo">Movie Explorer</div>
                         <nav className="main-nav">
-                            <a href="#" className="nav-item">Movies</a>
-                            <a href="#" className="nav-item">TV Shows</a>
-                            <a href="#" className="nav-item more-item">More</a>
+                            {/* Mengubah link menjadi div/button agar bisa memanggil handler */}
+                            <div className="nav-item-functional" onClick={() => handleNavClick('Movies')}>Movies</div>
+                            <div className="nav-item-functional" onClick={() => handleNavClick('TV Shows')}>TV Shows</div>
+                            <div className="nav-item-functional" onClick={() => handleNavClick('More')}>More</div>
                         </nav>
                     </div>
                     <div className="navbar-right">
@@ -26,21 +33,13 @@ function AppHeader({ onToggleFilter, isFilterActive }) {
                         >
                             🔍
                         </button>
-                        <a href="#" className="nav-item">Login</a>
-                        {}
-                        {}
+                        {/* Tombol Login */}
+                        <div className="nav-item-functional" onClick={() => handleNavClick('Login')}>Login</div>
+                        {/* Tombol Join TMDB dihapus */}
                     </div>
                 </div>
             </div>
-
-            {}
-            <div className="hero-overlay"> 
-                <div className="container header-content">
-                    <div className="hero-text">
-                        {}
-                    </div>
-                </div>
-            </div>
+            
         </header>
     );
 }
